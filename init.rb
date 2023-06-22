ecb83699c35d4768e494c00306d8ddc9c7730a1f
yui-har <7232538+yui-har@users.noreply.github.com>
2023-06-22 16:51:20 +0900

    Requires Redmine 5.0.5 or higher

--- init.rb
+++ init.rb
@@ -16,7 +16,7 @@ Redmine::Plugin.register :redmica_s3 do
   author_url 'https://www.farend.co.jp'
 
   version '1.0.12'
-  requires_redmine version_or_higher: '4.1.0'
+  requires_redmine version_or_higher: '5.0.5'
 
   Redmine::Thumbnail.__send__(:include, RedmicaS3::ThumbnailPatch)
   Redmine::Utils.__send__(:include, RedmicaS3::UtilsPatch)
