06eddfcd847bc6402e64a724f935d8ea1919d7e8
yui-har <7232538+yui-har@users.noreply.github.com>
2023-06-26 12:01:36 +0900

    Bump version to 2.0.0

--- init.rb
+++ init.rb
@@ -15,7 +15,7 @@ Redmine::Plugin.register :redmica_s3 do
   author 'Far End Technologies Corporation'
   author_url 'https://www.farend.co.jp'
 
-  version '1.0.12'
+  version '2.0.0'
   requires_redmine version_or_higher: '5.0.5'
 
   Redmine::Thumbnail.__send__(:include, RedmicaS3::ThumbnailPatch)
