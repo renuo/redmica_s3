8bf14e2d1d051725d1ab67e605fb391546948d2b
yui-har <7232538+yui-har@users.noreply.github.com>
2025-01-15 17:25:35 +0900

    Replaces use of Digest::MD5 / Digest::SHA1 with ActiveSupport::Digest (#35217). https://svn.redmine.org/redmine/trunk@22816

--- lib/redmica_s3/attachment_patch.rb
+++ lib/redmica_s3/attachment_patch.rb
@@ -32,7 +32,7 @@ module RedmicaS3
           if %r{^[a-zA-Z0-9_\.\-]*$}.match?(filename) && filename.length <= 50
             ascii = filename
           else
-            ascii = Digest::MD5.hexdigest(filename)
+            ascii = ActiveSupport::Digest.hexdigest(filename)
             # keep the extension if any
             ascii << $1 if filename =~ %r{(\.[a-zA-Z0-9]+)$}
           end
@@ -86,7 +86,7 @@ module RedmicaS3
       end
 
       # Copies the temporary file to its final location
-      # and computes its MD5 hash
+      # and computes its hash
       def files_to_final_location
         if @temp_file
           self.disk_directory = target_directory
